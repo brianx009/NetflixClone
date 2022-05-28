6547da0f3581421d1a2f074867913d34632e74fb
BrianSaldana <brianxsaldana@gmail.com>
2022-05-27 18:28:22 -0700

    Login with JWT Authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,6 +4,8 @@ const { response } = require('express');
 const User = require("../models/User")
 //npm install crypto js in order to make a created users password encrypted and not a visible string
 const CryptoJS = require("crypto-js");
+// JsonWebToken in order to create an access token for users, making CRUD process easier
+const jwt = require("jsonwebtoken");
 
 //Register
 //post - for creating
@@ -31,5 +33,42 @@ router.post("/register", async (req, res) =>{
         }
 });
 
+//Login
+//taking in users email and password and sending them back a response if they are a user existing in our DB.
+router.post("/login", async (req,res)=>{
+    try{
+        //try to find user based on the email provided
+        //condition states "if email = req.body.email" (signed up by user), it means we have that user
+        const user = await User.findOne({email: req.body.email});
+        //if there is no user, send an error message
+        !user && res.status(401).json("Wrong Email or Password, please try again.")
+
+        //decryption of the password in MongoDB by using Crypto JS , and check to see if the password submitted is the same
+        //as the password we set up in Postman
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        //if original password does not equal the reqested password from Postman then we send a error message
+        originalPassword !== req.body.password && 
+            res.status(401).json("Wrong Email or Password, please try again.")
+
+        // access token that will be created which will hold the user id and admin status. Hides the information inside the token, and for ease of  use
+        //we are also using the same Secret key we used earlier in our application. Also set up to expire in 5 days, after 5 days it will ask us to login again. 
+        const accessToken = jwt.sign(
+            {id: user._id, isAdmin: user.isAdmin},
+            process.env.SECRET_KEY,
+            {expiresIn: "5d"}
+        );
+        //this removes any password information that might show up when we use Postman to check if the username/password is correct 
+        const{ password, ...info} = user._doc;
+
+        // else if they do match then we send a successful status 200 and access token which holds information about the user
+        res.status(200).json({...info, accessToken})
+
+    }catch(err){
+        //error message 500
+        res.status(500).json(err)
+    }
+})
 //exports our router
 module.exports = router;
