ddc9aab54cb3c350076d9b7349bbcc55f60bcc2c
BrianSaldana <brianxsaldana@gmail.com>
2022-06-09 01:17:47 -0700

    Featured Page in progress save point

---
+++ myapp/src/components/featured/Featured.jsx
@@ -0,0 +1,63 @@
+import { BsFillPlayFill } from "react-icons/bs";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+
+import "./featured.scss";
+
+export default function Featured({ type }) {
+  return (
+    <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
+      {/* movie thumbnail */}
+      <img
+        src="images/movie/CC.png"
+        alt=""
+      />
+      {/*Movie Title png */}
+      <div className="info">
+        <img
+          src="images/movieTitle/CC_GFN.png"
+          alt=""
+        />
+        {/*Movie Description */}
+        <span className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          adipisci repellendus eum quasi illo, velit numquam, maxime tempora
+          sint deleniti, aliquid qui? Facilis, adipisci! Ratione hic repudiandae
+          temporibus eum earum?
+        </span>
+        {/*Play Button Icon */}
+        <div className="buttons">
+          <button className="play">
+            <BsFillPlayFill/>
+            <span>Play</span>
+          </button>
+          {/*Information button Icon */}
+          <button className="more">
+            <AiOutlineInfoCircle/>
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- myapp/src/home/Home.jsx
+++ myapp/src/home/Home.jsx
@@ -2,12 +2,15 @@
 import "./home.scss"
 //importing Navbar so we could call it and have it display in our home page
 import Navbar from "../components/navbar/Navbar"
+import Featured from "../components/featured/Featured"
 
 //exports home page with navbar component
 export default function Home() {
   return (
     <div className="home">
+      {/*calls our navbar component to our home page */}
       <Navbar/>
+      <Featured/>
     </div>
   )
 }
