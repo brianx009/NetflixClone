ef6a30d312e11eb1016ebbde8ce4a92d174045f9
BrianSaldana <brianxsaldana@gmail.com>
2022-06-16 13:22:30 -0700

    React Login page -- tested and working.

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.scss";
-import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+//import Register from "./pages/register/Register";
 //import Home from "./pages/home/Home";
 //import Watch from "./pages/watch/Watch";
 
 const App = () => {
-  return <Register/>;
+  return <Login/>;
 };
 
 export default App;
