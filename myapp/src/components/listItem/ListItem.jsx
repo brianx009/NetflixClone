ee1081e135a14e4fcea32e9878d01b0a0f31ce20
BrianSaldana <brianxsaldana@gmail.com>
2022-06-22 18:33:49 -0700

    Displaying Movies from the API - Successful and Working

--- myapp/src/components/listItem/ListItem.jsx
+++ myapp/src/components/listItem/ListItem.jsx
@@ -2,12 +2,29 @@ import "./listItem.scss";
 import { BsFillPlayFill } from "react-icons/bs";
 import { IoIosAdd } from "react-icons/io";
 import { BiLike, BiDislike } from "react-icons/bi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function ListItem({ index }) {
+export default function ListItem({ index, item }) {
   const [isHovered, setIsHovered] = useState(false);
-  const trailer =
-    "https://ia802702.us.archive.org/14/items/CC_1914_08_31_TheGoodforNothing/CC_1914_08_31_TheGoodforNothing_512kb.mp4";
+  const [movie, setMovie] = useState(false);
+
+  useEffect(()=> {
+    const getMovie = async ()=>{
+      try{
+        const res = await axios.get("/movies/find/" + item,{
+          headers:{
+            token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOTE2ZTU2NzYxZDA1ZDRjODViYzc2ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NTkzOTY5OSwiZXhwIjoxNjU2MzcxNjk5fQ.w7QBBUPRxbfmJDvA2jL4IFu241MW9stVSopQFlaZIqo"
+          },
+        });
+        setMovie(res.data);
+      }catch(err){
+        console.log(err)
+      }
+    };
+    getMovie()
+  }, [item]);
+
   return (
     <div
       className="listItem"
@@ -16,12 +33,12 @@ export default function ListItem({ index }) {
       onMouseLeave={() => setIsHovered(false)}
     >
       <img
-        src ="images/movie/CC.png"
+        src ={movie.image}
         alt=""
       />
       {isHovered && (
         <>
-          <video src={trailer} autoPlay={true} loop />
+          <video src={movie.trailer} autoPlay={true} loop />
           <div className="itemInfo">
             {/*icons for play, like, dislike, and add movie */}
             <div className="icons">
@@ -32,21 +49,19 @@ export default function ListItem({ index }) {
             </div>
             <div className="itemInfoTop">
               {/*time limit of movie */}
-              <span>13 Minuets 30 Seconds</span>
+              <span>{movie.duration}</span>
               {/*Age limit recstrictions */}
-              <span className="limit">G</span>
+              <span className="limit">{movie.limit}</span>
               {/*year released */}
-              <span>Aug. 31 1914</span>
+              <span>{movie.year}</span>
             </div>
             {/*description of the movie */}
             <div className="desc">
-              Charlie Chaplins 25th Film As His New Profession 
-              An American comedy silent film made at the Keystone 
-              Studios and starring Charlie Chaplin.
+              {movie.desc}
             </div>
             {/* Movie Genre */}
             <div className="genre">
-              Comedy
+              {movie.genre}
             </div>
           </div>
         </>
